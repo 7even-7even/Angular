5cdad8fbd655100c494909738ec98a2789f714fd
7even-7even <sidxohal9049@gmail.com>
2025-02-18 12:35:59 +0530

    Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'TIB37';
+  displayname!: string;
+  displayaddress!: string;
+  displayemail!: string;
+  displaycontact!: string;
+
+
+  getvalue(name: string, address: string, contact: string, email: string) {
+    this.displayname = name;
+    this.displayaddress = address;
+    this.displayemail = email;
+    this.displaycontact = contact;
+  }
+}
+
